90cd866ee90ac9c09dbfa79d1a38e2390e075eff
Leon Brüwer <leon.bruewer@web.de>
2018-09-06 08:46:20 +0200

    created table

---
+++ Training-LR-Company/Address.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [PostalCode] INT NOT NULL, 
+    [Place] NVARCHAR(256) NULL, 
+    [Street] NVARCHAR(256) NULL,
+	[CreationTime] DATETIME2(7) NOT NULL DEFAULT GETDATE(),
+	[DeleteTime] DATETIME2(7) NULL
+)
