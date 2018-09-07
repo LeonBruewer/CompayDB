abbd1aa85eec391663639eb4930ccffab0b598fe
Leon Brüwer <leon.bruewer@web.de>
2018-09-07 10:24:31 +0200

    created add or update logic for address stored procedure

--- Training-LR-Company/Procedures/spInsertAddress.sql
+++ Training-LR-Company/Procedures/spInsertAddress.sql
@@ -1,7 +1,13 @@
 ﻿CREATE PROCEDURE [dbo].[spAddOrUpdateAddress]
+	@Id int NULL,
 	@PostalCode int,
 	@Street nvarchar(256)
 AS
+IF @Id = null
 	INSERT INTO Address(PostalCode, Street)
 	VALUES(@PostalCode, @Street)
+ELSE
+	UPDATE Address
+	SET PostalCode = @PostalCode, Street = @Street
+	WHERE Id = @Id
 RETURN 0
