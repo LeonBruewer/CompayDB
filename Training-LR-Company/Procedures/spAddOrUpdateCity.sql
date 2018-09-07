cc038f801d183b33f7da4fabfa49d18eb52a7de5
Leon Brüwer <leon.bruewer@web.de>
2018-09-07 09:37:18 +0200

    create procedure to add or update row

---
+++ Training-LR-Company/Procedures/spAddOrUpdateCity.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spAddOrUpdateCity]
+	@PostalCode int = 0,
+	@City int
+AS
+	IF ((SELECT COUNT(PostalCode) FROM City) <= 0)
+		INSERT INTO City(PostalCode, City)
+		VALUES(@PostalCode, @City)
+	ELSE
+		UPDATE City
+		SET City = @City
+		WHERE PostalCode = @PostalCode
+RETURN 0
