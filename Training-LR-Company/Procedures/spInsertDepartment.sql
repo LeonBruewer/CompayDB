9ec82e527f61617b7ccc71d46a1cef9bb2e70ceb
Leon Brüwer <leon.bruewer@web.de>
2018-09-07 10:30:38 +0200

    created add or update logic for department stored procedure

--- Training-LR-Company/Procedures/spInsertDepartment.sql
+++ Training-LR-Company/Procedures/spInsertDepartment.sql
@@ -1,8 +1,14 @@
 ﻿CREATE PROCEDURE [dbo].[spAddOrUpdateDepartment]
+	@Id int null,
 	@DepartmentName nvarchar(256),
 	@CompanyId int,
 	@ManagerId int
 AS
+IF @Id = null
 	INSERT INTO Department(DepartmentName, CompanyId, ManagerId)
 	VALUES (@DepartmentName, @CompanyId, @ManagerId)
+ELSE
+	UPDATE Department
+	SET DepartmentName = @DepartmentName, CompanyId = @CompanyId, ManagerId = @ManagerId
+	WHERE Id = @Id
 RETURN 0
