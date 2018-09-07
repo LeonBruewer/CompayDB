72c03469baa9a2dd6b4bde2268ce6302a112e65c
Leon Brüwer <leon.bruewer@web.de>
2018-09-07 10:28:21 +0200

    created add or update logic for company stored procedure

--- Training-LR-Company/Procedures/spInsertCompany.sql
+++ Training-LR-Company/Procedures/spInsertCompany.sql
@@ -1,7 +1,13 @@
 ﻿CREATE PROCEDURE [dbo].[spAddOrUpdateCompany]
+	@Id int null,
 	@CompanyName nvarchar(256),
 	@AddressId int
 AS
+IF @Id = null
 	INSERT INTO Company(CompanyName, AddressId)
 	VALUES(@CompanyName, @AddressId)
+ELSE
+	Update Company
+	SET CompanyName = @CompanyName, AddressId = @AddressId
+	WHERE Id = @Id
 RETURN 0
