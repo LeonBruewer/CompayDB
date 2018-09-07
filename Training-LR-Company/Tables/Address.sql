7551e3ac1c19a1b9a8bc86ccf6b0227a00bf7884
Leon Brüwer <leon.bruewer@web.de>
2018-09-07 09:28:58 +0200

    removed city column from address table and referenced postalcode to city

--- Training-LR-Company/Tables/Address.sql
+++ Training-LR-Company/Tables/Address.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[Address]
 (
 	[Id] INT NOT NULL  IDENTITY, 
-    [PostalCode] INT NOT NULL, 
-    [City] NVARCHAR(256) NULL, 
+    [PostalCode] INT NOT NULL REFERENCES [City](PostalCode),
     [Street] NVARCHAR(256) NULL,
 	[CreationTime] DATETIME2(7) NOT NULL DEFAULT GETDATE(),
 	[DeleteTime] DATETIME2(7) NULL, 
