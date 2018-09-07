1dee1da5ebf31d0935619b6f248ffb5d60b1da8e
Leon Brüwer <leon.bruewer@web.de>
2018-09-07 10:33:17 +0200

    created add or update logic for eployee stored procedure

--- Training-LR-Company/Procedures/spInsertEmployee.sql
+++ Training-LR-Company/Procedures/spInsertEmployee.sql
@@ -1,4 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[spAddOrUpdateEmployee]
+	@Id int null,
 	@FirstName nvarchar(256),
 	@Surname nvarchar(256),
 	@BirthDate datetime2(7),
@@ -8,6 +9,11 @@
 	@CompanyId int,
 	@AddressId int
 AS
+IF @Id = null
 	INSERT INTO Employee(FirstName, Surname, BirthDate, Gender, PhoneNumber, DepartmentId, CompanyId, AddressId)
 	VALUES(@FirstName, @Surname, @BirthDate, @Gender, @PhoneNumber, @DepartmentId, @CompanyId, @AddressId)
+ELSE
+	UPDATE Employee
+	SET FirstName = @FirstName, Surname = @Surname, BirthDate = @BirthDate, Gender = @Gender, PhoneNumber = @PhoneNumber, DepartmentId = @DepartmentId, CompanyId = @CompanyId, AddressId = @AddressId
+	WHERE Id = @Id
 RETURN 0
